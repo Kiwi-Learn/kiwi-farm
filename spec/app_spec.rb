71152659a2ff1bd8ae71684c7caa129e572438a7
John-Lin <linton.tw@gmail.com>
2015-10-24 15:14:37 +0800

    fixed test case

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -7,11 +7,12 @@ describe 'Getting the root of the service' do
     last_response.must_be :ok?
     last_response.body.must_match(/Kiwi farm service/)
   end
- 
+end
+
+describe 'Getting the detail of course' do
   it 'Should return ok' do
     get '/api/v1/courses/info/MA02004'
     last_response.must_be :ok?
     last_response.body.must_match(/"id":"MA02004"/)
   end
-
 end
